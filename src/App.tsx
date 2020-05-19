2509a11d994a018b33c4e349f4a41e4ee219df88
wcastand <william@paybase.io>
2020-05-19 15:59:04 +0100

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Normalize } from 'styled-normalize';
 
 interface AppProps {}
 
 function App({}: React.Props<AppProps>) {
   return (
-    <div className={'App'}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Normalize />
+      <div>
+        <header>
+          <p>
+            Edit <code>src/as.tsx</code> and save to reload.
+          </p>
+          <a
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </>
   );
 }
 
--- types/static.d.ts
+++ types/static.d.ts
@@ -1,10 +1,18 @@
 /* Use this file to declare any custom file extensions for importing */
 /* Use this folder to add/extend a package d.ts file, if needed. */
+import {} from 'styled-components';
+import { CSSProp } from 'styled-components';
 
-declare module "*.svg" {
+declare module '*.svg' {
   const content: string;
   export default content;
 }
-declare module "*.css";
-declare module "*.png";
-declare module "*.jpg";
+declare module '*.css';
+declare module '*.png';
+declare module '*.jpg';
+declare module 'styled-reset';
+declare module 'react' {
+  interface Attributes {
+    css?: CSSProp | CSSObject;
+  }
+}
